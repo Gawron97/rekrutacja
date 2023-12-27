03877432b5da52a36353ad00ba7480a06cbe5584
lukcyn <lukaszcyndzerr@gmail.com>
2023-12-27 18:39:08 +0100

    Frontend integration

--- src/main/java/com/example/rekrutacja/repository/AppUserRepository.java
+++ src/main/java/com/example/rekrutacja/repository/AppUserRepository.java
@@ -3,5 +3,9 @@ package com.example.rekrutacja.repository;
 import com.example.rekrutacja.entity.users.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findAppUserByLogin(String login);
 }
