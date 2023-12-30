2866e30df83f553dd8bc43b2cbe89daf794ca43a
lukcyn <lukaszcyndzerr@gmail.com>
2023-12-30 20:46:28 +0100

    Added filter to allow authenticated users to use secured endpoints

--- src/main/java/com/example/rekrutacja/controller/TestController.java
+++ src/main/java/com/example/rekrutacja/controller/TestController.java
@@ -13,4 +13,9 @@ public class TestController {
     public String test() {
         return "test";
     }
+
+    @GetMapping("/secured")
+    public String testSecured() {
+        return "test secured";
+    }
 }
---
+++ src/main/java/com/example/rekrutacja/utils/config/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.example.rekrutacja.utils.config.filter;
+
+import com.example.rekrutacja.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter  extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authHeader.substring(7);
+        UserDetails user = userDetailsService.loadUserByUsername(jwtService.extractUsername(jwt));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                new ArrayList<>()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
